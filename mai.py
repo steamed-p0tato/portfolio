37a2b64da4c5d50cef1324604ed54f988bd259fd
Ariyan Basu <126325723+steamed-p0tato@users.noreply.github.com>
2024-08-17 11:49:23 +0530

    Add files via upload

---
+++ mai.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(page_title="Arii", page_icon=":slightly_smiling_face:", layout="wide")
+
+#header 
+st.subheader("hi I am Ariyan :wave:")
+st.subheader("A Machine learning researcher from India")
+st.write("I thrive in experimentation and finding new tools and new ways to use software and hardware to try and make life better")
+st.write("[github](https://github.com/steamed-p0tato?tab=repositories)")
+
+with st.container():
+    st.subheader("ongoing projects and research:")
+    st.write("ICSSR language preservation project- the goal of this project is to preserve a language called Toto relevent in the Indo bhutanese border in a place called Toto Para")
+    st.write("Glucoma detection- the aim is to make an hardware and software combo which would detect early signs of Glucoma in order to remedy with quick treatment")
+    st.write("Automated trading AI- to trade stocks when am not physically present")
+    st.write("smart greenhouse- a farm managment system that uses simple ML algorithms to automate and predict the outcomes in a farming environment")
+    st.write("[greenhouse](https://github.com/steamed-p0tato/plant-disease-identification)")
